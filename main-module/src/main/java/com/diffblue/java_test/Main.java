1ec36fa7a3f0db0e14f68e45febe0f0b52996590
Peter Schrammel <peter.schrammel@cs.ox.ac.uk>
2016-08-28 19:02:24 +0100

    check for div by 0

--- main-module/src/main/java/com/diffblue/java_test/Main.java
+++ main-module/src/main/java/com/diffblue/java_test/Main.java
@@ -6,8 +6,13 @@ public class Main
   {
     int x=Integer.parseInt(a);
     int y=Integer.parseInt(b);
-    int z=new Calc().div(x,y);
-    System.out.println(""+z);
+    if(y!=0)
+    {
+      int z=new Calc().div(x,y);
+      System.out.println(""+z);
+    }
+    else
+      System.out.println("second argument must not be zero");
   }
 
   public static void main(String[] args)
