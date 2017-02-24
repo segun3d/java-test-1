954adc9fff442f3ec21c439cdcbb0b11bc8def50
Robert (Jamie) Munro <jamie@diffblue.com>
2017-02-24 15:03:29 +0000

    Eclipse auto-fixes

--- main-module/src/test/java/com/diffblue/java_test/AdditionsTest.java
+++ main-module/src/test/java/com/diffblue/java_test/AdditionsTest.java
@@ -1,58 +1,63 @@
 package com.diffblue.java_test;
 
-import org.junit.*;
 import static org.junit.Assert.assertEquals;
+
 import com.diffblue.java_testcase.Reflector;
 
-public class AdditionsTest
-{
+public class AdditionsTest {
 
-/* This test case reaches the following goals:
- * function java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block 21
- *  com/diffblue/java_test/Additions.java:41
- * function java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block 22
- *  com/diffblue/java_test/Additions.java:42
- * function java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block 23
- *  com/diffblue/java_test/Additions.java:43
- * function java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block 24
- *  com/diffblue/java_test/Additions.java:44
- */
+  /*
+   * This test case reaches the following goals: function
+   * java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block
+   * 21 com/diffblue/java_test/Additions.java:41 function
+   * java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block
+   * 22 com/diffblue/java_test/Additions.java:42 function
+   * java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block
+   * 23 com/diffblue/java_test/Additions.java:43 function
+   * java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block
+   * 24 com/diffblue/java_test/Additions.java:44
+   */
 
-@org.junit.Test
-public void com_diffblue_java_test_Additions_checkTicTacToePosition_100504ff5d66aea9_002() throws Exception {
+  @org.junit.Test
+  public void com_diffblue_java_test_Additions_checkTicTacToePosition_100504ff5d66aea9_002() throws Exception {
     /* initialize test parameters */
-    com.diffblue.java_test.Additions tmp_object_factory$26=(com.diffblue.java_test.Additions) Reflector.forceInstance("com.diffblue.java_test.Additions");
-    byte [] dynamic_2_array=new byte []{ (byte)0, (byte)1, (byte)2, (byte)0, (byte)1, (byte)2, (byte)0, (byte)1, (byte)0 };
-    byte [] dynamic_object1=(byte [])dynamic_2_array;
-    byte [] a=(byte [])dynamic_object1;
+    com.diffblue.java_test.Additions tmp_object_factory$26 = (com.diffblue.java_test.Additions) Reflector
+        .forceInstance("com.diffblue.java_test.Additions");
+    byte[] dynamic_2_array = new byte[] { (byte) 0, (byte) 1, (byte) 2, (byte) 0, (byte) 1, (byte) 2, (byte) 0,
+        (byte) 1, (byte) 0 };
+    byte[] dynamic_object1 = dynamic_2_array;
+    byte[] a = dynamic_object1;
 
     /* call function under test */
     byte retval;
     retval = tmp_object_factory$26.checkTicTacToePosition(a);
 
     /* check return value */
-    assertEquals(retval, (byte)1);
+    assertEquals(retval, (byte) 1);
   }
 
-  /* This test case reaches the following goals:
- * function java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block 16
- *  com/diffblue/java_test/Additions.java:35
- */
+  /*
+   * This test case reaches the following goals: function
+   * java::com.diffblue.java_test.Additions.checkTicTacToePosition:([B)B block
+   * 16 com/diffblue/java_test/Additions.java:35
+   */
 
-@org.junit.Test
-public void com_diffblue_java_test_Additions_checkTicTacToePosition_100504ff5d66aea9_009() throws Exception {
+  @org.junit.Test
+  public void com_diffblue_java_test_Additions_checkTicTacToePosition_100504ff5d66aea9_009() throws Exception {
     /* initialize test parameters */
-    com.diffblue.java_test.Additions tmp_object_factory$26=(com.diffblue.java_test.Additions) Reflector.forceInstance("com.diffblue.java_test.Additions");
-    byte [] dynamic_2_array=new byte []{ (byte)0, (byte)2, (byte)0, (byte)0, (byte)1, (byte)1, (byte)1, (byte)1, (byte)0 };
-    byte [] dynamic_object1=(byte [])dynamic_2_array;
-    byte [] a=(byte [])dynamic_object1;
+    com.diffblue.java_test.Additions tmp_object_factory$26 = (com.diffblue.java_test.Additions) Reflector
+        .forceInstance("com.diffblue.java_test.Additions");
+    byte[] dynamic_2_array = new byte[] { (byte) 0, (byte) 2, (byte) 0, (byte) 0, (byte) 1, (byte) 1, (byte) 1,
+        (byte) 1, (byte) 0 };
+    byte[] dynamic_object1 = dynamic_2_array;
+    byte[] a = dynamic_object1;
 
     /* call function under test */
     byte retval;
     retval = tmp_object_factory$26.checkTicTacToePosition(a);
 
     /* check return value */
-    assertEquals(retval, (byte)-1);
+    assertEquals(retval, (byte) -1);
   }
 
 }
