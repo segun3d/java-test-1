f27f0f08e9fde2fb30c2be6d01a89d503d1b1774
Robert (Jamie) Munro <jamie@diffblue.com>
2017-04-25 16:44:28 +0100

    Add a (very) basic string calculator

--- main-module/src/main/java/com/diffblue/javatest/Calc.java
+++ main-module/src/main/java/com/diffblue/javatest/Calc.java
@@ -4,4 +4,29 @@ public class Calc {
   static int div(int a, int b) {
     return a / b;
   }
+
+  static int calc(String str)
+  {
+    if (str.contains("-")) {
+      String[] arr = str.split("-", 1);
+      return calc(arr[0]) - calc(arr[1]);
+    }
+
+    if (str.contains("+")) {
+      String[] arr = str.split("+", 1);
+      return calc(arr[0]) + calc(arr[1]);
+    }
+
+    if (str.contains("*")) {
+      String[] arr = str.split("*", 1);
+      return calc(arr[0]) * calc(arr[1]);
+    }
+
+    if (str.contains("/")) {
+      String[] arr = str.split("/", 1);
+      return calc(arr[0]) / calc(arr[1]);
+    }
+
+    return new Integer(str);
+  }
 }
