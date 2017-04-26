2f823dcba33ea4b0f754433bf0fcf36e4bfff65d
Robert (Jamie) Munro <jamie@diffblue.com>
2017-04-26 17:49:43 +0100

    Add loads more example string functions

--- main-module/src/main/java/com/diffblue/javatest/StringStuff.java
+++ main-module/src/main/java/com/diffblue/javatest/StringStuff.java
@@ -1,8 +1,141 @@
 package com.diffblue.javatest;
 
+import java.io.PrintStream;
 import java.util.Locale;
 
 public class StringStuff {
+
+  public static String classGetNameExample(Class c) {
+    StringStuff t = new StringStuff();
+    Class testClass = t.getClass();
+    if (c.getName().equals(testClass.getName()))
+      return c.getName();
+    return "X";
+  }
+
+  public static String classGetSimpleNameExample(Class c) {
+    StringStuff t = new StringStuff();
+    Class testClass = t.getClass();
+    if (c.getName().equals(testClass.getName()))
+      return c.getSimpleName();
+    return "X";
+  }
+
+  public static int integerParseIntExample(String s) {
+    int i = Integer.parseInt(s);
+    if (i == 4)
+      return i;
+    else if (i == -10)
+      return i;
+    else
+      return i;
+  }
+
+  public static String objectToStringExample(int i) {
+    Object obj;
+    if (i == 1)
+      obj = new Integer(1);
+    else if (i == 2)
+      obj = new String("2");
+    else
+      obj = new StringBuilder("3");
+    return obj.toString();
+  }
+
+  public static void printStreamExample(String s) {
+    PrintStream ps = new PrintStream(System.out);
+    ps.println(s);
+  }
+
+  public static StringBuffer stringBufferAppendExample(String s) {
+    StringBuffer sb = new StringBuffer();
+    sb.append(s);
+    sb.append("=");
+    sb.append(s);
+    return sb;
+  }
+
+  public static StringBuilder stringBuilderAppendExample(String s) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(s);
+    sb.append("=");
+    sb.append(s);
+    return sb;
+  }
+
+  public static int stringBuilderAppend2Example(StringBuilder sb) {
+    sb.append("!");
+    int size = sb.toString().length();
+
+    if (size >= 3) {
+      return 3;
+    } else
+      return 0;
+  }
+
+  public static String stringBuilderAppend3Example(String s) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(s);
+    sb.append("=");
+    sb.append(s);
+    String ret = sb.toString();
+    return ret;
+  }
+
+  public static String stringBuilderAppendCExample(char c) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(c);
+    sb.append('=');
+    sb.append(c);
+    return sb.toString();
+  }
+
+  public static String stringBuilderAppendDExample(double i) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(i);
+    sb.append("=");
+    sb.append(i);
+    return sb.toString();
+  }
+
+  public static String stringBuilderAppendFExample(float f) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("XYZ");
+    sb.append(f);
+    return sb.toString();
+  }
+
+  public static String stringBuilderAppendIExample(int i) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(i);
+    sb.append("=");
+    sb.append(i);
+    return sb.toString();
+  }
+
+  public static String stringBuilderAppendJExample(long l) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("x");
+    sb.append(l);
+    return sb.toString();
+  }
+
+  public static int stringBuilderAppendJ2Example(long l) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(l);
+    if (l <= 1000L)
+      return 2;
+    else
+      return 1;
+  }
+
+  public static String stringBuilderAppendObjectExample(Object obj) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(obj);
+    sb.append("=");
+    sb.append(obj);
+    return sb.toString();
+  }
 
   public static StringBuilder stringBuilderAppendZExample(Boolean x) {
     StringBuilder sb = new StringBuilder();
@@ -105,5 +238,109 @@ public class StringStuff {
       return 1;
     else
       return -1;
+  }
+  // Test name 
+
+  public static int stringLengthExample(String s) {
+    if (s.length() == 2)
+      return 2;
+    if (s.length() == 4)
+      return 4;
+    return s.length() + 1;
+  }
+  // Test name 
+
+  public static int stringStartsWithExample(String s) {
+    if (s.startsWith("hel")) {
+      return 1;
+    }
+    return 2;
+  }
+  // Test name 
+
+  public static int stringStartsWith2Example(String s) {
+    if (s.startsWith("hello")) {
+      if (s.length() == 10)
+        return 0;
+      else
+        return 1;
+    }
+    if (s.startsWith("hel"))
+      return 2;
+    else
+      return 3;
+  }
+  // Test name 
+
+  public static String stringSubstringIIExample(String s) {
+    if (s.length() <= 10)
+      return s;
+    else
+      return s.substring(2, 5);
+  }
+  // Test name 
+
+  public static char[] stringToCharArrayExample(String s) {
+    assert (s.length() >= 2);
+    return s.toCharArray();
+  }
+  // Test name 
+
+  static int stringToLowerCaseExample(String s) {
+    if (s.equals("abc"))
+      return 0;
+
+    String a = s.toLowerCase();
+    if (a.equals("abc"))
+      return 1;
+
+    return 2;
+  }
+  // Test name 
+
+  public static String stringTrimExample(String s) {
+    String t = s.trim();
+    if (t.length() == s.length() - 3)
+      return t;
+    else
+      return s;
+  }
+
+  public static int checkHtml(String s) {
+    int p = s.indexOf('<');
+    if (s.length() > 30)
+      return -1; // we want a short example
+
+    if (p == -1)
+      return 0;
+
+    if (!s.startsWith("<html>", p))
+      return 0;
+
+    int q = s.lastIndexOf('<');
+
+    if (q == -1)
+      return 0;
+
+    if (s.startsWith("</html>", q)) {
+      assert (false);
+      return 1;
+    } else{
+      return 0;}
+  }
+
+  public static int checkHtml2(String s) {
+    if (s.length() > 30)
+      return -1; // we want a short example
+
+    String t = s.trim();
+
+    if (!t.startsWith("<html>"))
+      return 0;
+
+    if (!t.endsWith("</html>"))
+      return 0;
+
+    return 1;
   }
 }
