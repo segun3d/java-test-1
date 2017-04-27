e232ea3928f5e0b8cdba40bbdb53b2515b55fe63
Robert (Jamie) Munro <jamie@diffblue.com>
2017-04-27 14:25:22 +0100

    Add some tests for string calc

--- main-module/src/test/java/com/diffblue/javatest/CalcTest.java
+++ main-module/src/test/java/com/diffblue/javatest/CalcTest.java
@@ -9,4 +9,58 @@ public class CalcTest {
     int result = new Calc().div(2, 1);
     Assert.assertEquals(result, 2);
   }
+
+  @Test
+  public void calcAdd() {
+    int r = Calc.calc("10+5");
+    Assert.assertEquals(10+5, r);
+  }
+
+  @Test
+  public void calcSub() {
+    int r = Calc.calc("10-5");
+    Assert.assertEquals(10-5, r);
+  }
+
+  @Test
+  public void calcSubAdd() {
+    int r = Calc.calc("10-5+3");
+    Assert.assertEquals(10-5+3, r);
+  }
+
+  @Test
+  public void calcMul() {
+    int r = Calc.calc("10*5");
+    Assert.assertEquals(10*5, r);
+  }
+
+  @Test
+  public void calcDiv() {
+    int r = Calc.calc("10*5");
+    Assert.assertEquals(10*5, r);
+  }
+
+  @Test
+  public void calcSubMulti() {
+    int r = Calc.calc("10-5-5");
+    Assert.assertEquals(10-5-5, r);
+  }
+
+  @Test
+  public void calcDivMulti() {
+    int r = Calc.calc("16/2/2");
+    Assert.assertEquals(16/2/2, r);
+  }
+
+  @Test
+  public void calcSubMul() {
+    int r = Calc.calc("10-5*2");
+    Assert.assertEquals(10-5*2, r);
+  }
+
+  @Test
+  public void calcSubMul2() {
+    int r = Calc.calc("10-5*2+1-4/2");
+    Assert.assertEquals(10-5*2+1-4/2, r);
+  }
 }
